19544eee184258f743c647afdd9a937245d93191
Artyom Perederiy <41237931+ArtyomPerederiy@users.noreply.github.com>
2019-06-07 00:37:27 +0300

    Update db_show.php

--- modules/db_show.php
+++ modules/db_show.php
@@ -12,17 +12,18 @@ mysqli_close($link);
 while ($news = mysqli_fetch_array($result)) {
 
     ?>
-    
-    <p><?php echo $news['topic'] . '<br>'; ?> </p>
+    <div class="card-body d-flex flex-column align-items-start">
+    <h3 class="mb-0"><p class='dark'><?php echo $news['topic'] . '<br>'; ?> </p></h3>
     <p><?php echo $news['content'] . '<br>'; ?></p>
     <p>Дата публикации: <?php echo $news['topicDate'] ?> / <?php echo $news['topicTime'] ?> </p>
     <p>Автор сообщения: <?php echo $news['author'] ?> </p>
     <br>
-    <a href="modules/db_delete.php?id=<?php echo $news['id']?>">Удалить новость</a>
-    <a href="modules/db_edit.php?id=<?php echo $news['id']?>">Изменить новость</a>
-
+    <a href="modules/db_delete.php?id=<?php echo $news['id']?>"><button type="button" class="btn btn-danger">Удалить новость</button></a>
+    <a href="modules/db_edit.php?id=<?php echo $news['id']?>"><button type="button" class="btn btn-dark">Изменить новость</button></a>
+    
     <br>
     <br>
+    </div>
     <?php
 
 }

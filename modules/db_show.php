6608382654032326bcacdd67247d6854e2513479
Artyom Perederiy <41237931+ArtyomPerederiy@users.noreply.github.com>
2019-06-06 23:28:26 +0300

    Update db_show.php

--- modules/db_show.php
+++ modules/db_show.php
@@ -15,11 +15,11 @@ while ($news = mysqli_fetch_array($result)) {
 
     <p><?php echo $news['topic'] . '<br>'; ?> </p>
     <p><?php echo $news['content'] . '<br>'; ?></p>
-    <p>Дата публикации: <?php echo $news['topicDate'] ?> / </p>
+    <p>Дата публикации: <?php echo $news['topicDate'] ?> / <?php echo $news['topicTime'] ?> </p>
     <p>Автор сообщения: <?php echo $news['author'] ?> </p>
     <br>
-    <a href="module/db_delete.php ? id=<?php echo $news['id']?>">Удалить новость</a>
-    <a href="module/db_edit.php ? id=<?php echo $news['id']?>">Изменить новость</a>
+    <a href="module/db_delete.php?id=<?php echo $news['id']?>">Удалить новость</a>
+    <a href="module/db_edit.php?id=<?php echo $news['id']?>">Изменить новость</a>
 
     <br>
     <br>
